d1819bca15e6ebd3a5735e5a1381bb0a79800dac
Grzegorz Bugajski <g.bugajski5@gmail.com>
2021-07-07 20:25:32 +0200

    update hello class

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("gałąź junior_feature");
+        System.out.println("test");
     }
 }
